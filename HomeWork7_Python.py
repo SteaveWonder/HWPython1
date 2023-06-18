f84d7b6273ee0226f41d21ab7d0595ff2657d0a7
Stepan <soulsteff@gmail.com>
2023-06-18 13:19:48 +0300

    HW7

--- HomeWork7_Python.py
+++ HomeWork7_Python.py
@@ -37,3 +37,15 @@ else:
 # 4 8 12 16 20 24
 # 5 10 15 20 25 30
 # 6 12 18 24 30 36
+
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    print('	'.join([str(i) for i in range(1, num_columns + 1)]))        # .join - принимает список и возвращает строку
+    for i in range(2, num_rows + 1):
+        print(i, end=' \t')
+        for j in range(2, num_columns + 1):
+            print(operation(i, j), end=' \t')
+        print()
+
+
+print_operation_table(lambda x, y: x * y)
+
